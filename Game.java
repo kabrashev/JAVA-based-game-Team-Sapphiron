7eee0f1623b9b2df6e84c59f5686d7acaa47673d
Petar Petrov <petrov90stz@gmail.com>
2016-04-04 11:35:22 +0300

    Added files via upload

---
+++ Game.java
@@ -0,0 +1,12 @@
+package JavaGame;
+
+import javax.swing.*;
+
+/**
+ * Created by petar on 2016-04-04.
+ */
+public class Game {
+    public static void main(String[] args) {
+        new Display();
+    }
+}
