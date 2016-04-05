f62a3aa702f3ad4e9ae5ab38f452840040adc968
Yonko Petrov <yonko.petrov@gmail.com>
2016-04-05 23:55:10 +0300

    I make some changes and add new files

---
+++ JavaGame/Game.java
@@ -0,0 +1,12 @@
+package JavaGame;
+
+import javax.swing.*;
+
+/**
+ * Предлагам от тук да тръгва приложението, като извиква останалите класове :) 
+ */
+public class Game {
+    public static void main(String[] args) {
+        new Display();
+    }
+}
