449bc66b78cb3cee6fcb4cd3120311cdf4f77eda
Deyan Svetoslavov <dsvetoslavov@gmail.com>
2016-04-08 02:01:13 +0300

    Updated Main  - some old code is now comment

--- JavaGame/Main.java
+++ JavaGame/Main.java
@@ -5,23 +5,27 @@ import java.awt.*;
 
 public class Main implements Constants {
     private static JFrame frame;
-    private static Board board;
-    private static Container container;
+    //private static Board board;
+    // GamePanel - maybe is like the board, but not sure
+    private static GamePanel gamePanel;  
+    //private static Container container;
 
     public static void main(String[] args){
 
         frame = new JFrame("Brick Breaker Game version 1.1.1");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        frame.setSize(Constants.WIDTH,Constants.HEIGHT);
         frame.setLocationRelativeTo(null);
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage("JavaGame/Images/Icon.png"));
-
-        board = new Board(WINDOW_WIDTH,WINDOW_HEIGHT);
-
-        container = frame.getContentPane();
-        container.add(board);
-
         frame.setVisible(true);
+        //adds the gamePanel to the frame
+        frame.add(gamePanel);
+        // board = new Board(WINDOW_WIDTH,WINDOW_HEIGHT);
+
+        // container = frame.getContentPane();
+        // container.add(board);
+
+       
     }
 }
