1be2b67ae7a8dda229226fcb10d245430f89feac
Kiril Abrashev <kabrashev@abv.bg>
2016-04-13 22:18:11 +0300

    Update file Constants.java. Set constants for bricks.

--- JavaGame/Constants.java
+++ JavaGame/Constants.java
@@ -1,8 +1,22 @@
 package JavaGame;
 
+import java.awt.*;
+
 public interface Constants {
     // Window size
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
 
+    //Size and number of bricks
+    public static final int BRICK_WIDTH = 50;
+    public static final int BRICK_HEIGHT = 25;
+    public static final int MAX_BRICKS = 80;
+    public static final int NO_BRICKS = 0;
+
+    //Brick Colors
+    public static final Color BLUE_BRICK = Color.blue;
+    public static final Color RED_BRICK = Color.red;
+    public static final Color ORANGE_BRICK = Color.orange;
+    public static final Color GREEN_BRICK = Color.green;
+    public static final Color YELLOW_BRICK = Color.yellow;
 }
