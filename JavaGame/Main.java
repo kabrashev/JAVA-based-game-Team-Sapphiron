59c30196b9892fe37a2db379e43d4fb7f02be131
boucheron <grifona00@gmail.com>
2016-04-13 22:53:59 +0300

    Fix typo
    Minor typo error

--- JavaGame/Main.java
+++ JavaGame/Main.java
@@ -1,7 +1,6 @@
 package JavaGame;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Main implements Constants {
     private static JFrame frame;
@@ -12,12 +11,26 @@ public class Main implements Constants {
 
     public static void main(String[] args){
 
+        // Splash screen
+        JWindow window = new JWindow();
+        window.getContentPane().add(new JLabel(new ImageIcon("images/icon.png"), SwingConstants.CENTER));
+        window.setBounds(0, 0, 72, 72);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        try
+        {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        window.setVisible(false);
+
         frame = new JFrame("Brick Breaker Game version 1.1.1");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setSize(Constants.WIDTH,Constants.HEIGHT);
         frame.setLocationRelativeTo(null);
-        frame.setIconImage(Toolkit.getDefaultToolkit().getImage("JavaGame/Images/Icon.png"));
+        frame.setIconImage(new ImageIcon("images/icon.png").getImage());
         frame.setVisible(true);
         //adds the gamePanel to the frame
         gamePanel = new GamePanel();
---
+++ out/production/JAVA-based-game-Team-Sapphiron/README.md
@@ -0,0 +1,2 @@
+# JAVA-based-game-Team-Sapphiron
+Teamwork Project: JAVA-based game (2016)
