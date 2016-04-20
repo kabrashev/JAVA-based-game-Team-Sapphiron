d5a9b45229e8b0480de82f5de299705f3ac57cbc
Petar Petrov <petrov90stz@gmail.com>
2016-04-20 22:13:15 +0300

    Added files via upload

--- JavaGame/GamePanel.java
+++ JavaGame/GamePanel.java
@@ -1,17 +1,16 @@
 package JavaGame;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import java.awt.image.BufferedImage;
-import java.util.Random;
 
-public class GamePanel extends JPanel implements Constants {
+        import javax.swing.*;
+        import java.awt.*;
+        import java.awt.event.MouseEvent;
+        import java.awt.event.MouseMotionListener;
+        import java.awt.image.BufferedImage;
+        import java.util.Random;
+
+public class GamePanel extends JPanel implements Runnable,Constants {
     //Fields running is true, when the game is running, image is what we show, g is drawing on the image
-    private boolean running;
+    private boolean isRunning;
     private BufferedImage image;
     private Graphics2D g;
     //add the ball and the paddle
@@ -20,11 +19,17 @@ public class GamePanel extends JPanel implements Constants {
     private Brick[][] brick = new Brick[16][5];
     private Color[] colors = {RED_BRICK, YELLOW_BRICK, GREEN_BRICK, ORANGE_BRICK, BLUE_BRICK};
     private int numberOfLives = PLAYER_LIVES;
+    private int score = 0;
+    private int level = 1;
+    private int bricksLeft = 16*5;
 
     private myMouseMotionListener mouseListener;
-    private BoardListener boardListener;
+
+    private Thread game;
 
     public GamePanel() {
+        super.setSize(800, 600);
+        setFocusable(true);
 
         createBricks();
 
@@ -32,13 +37,10 @@ public class GamePanel extends JPanel implements Constants {
 
         paddle = new Paddle();
 
-        running = true;
-
+        isRunning = true;
         //adding the mouse Listener
         mouseListener = new myMouseMotionListener();
-        boardListener = new BoardListener();
         addMouseMotionListener(mouseListener);
-        addKeyListener(boardListener);
 
         image = new BufferedImage(Constants.WIDTH, Constants.HEIGHT, BufferedImage.TYPE_INT_RGB);
 
@@ -46,7 +48,9 @@ public class GamePanel extends JPanel implements Constants {
 
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
+
     }
+
 
     private void createBricks() {
         for (int i = 0; i < 16; i++) {
@@ -59,25 +63,6 @@ public class GamePanel extends JPanel implements Constants {
     }
 
 
-    public void playGame() {
-        //Game Loop
-
-        while (running) {
-            //Update
-            update();
-            //Draw
-            draw();
-            //displayImage
-
-            checkIfOut(ball.getY());
-            repaint();
-            try {
-                Thread.sleep(5);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     public void checkCollisions() {
         Rectangle ballRect = ball.getRect();
@@ -90,19 +75,30 @@ public class GamePanel extends JPanel implements Constants {
     }
 
     public void checkBricks(int x1, int y1) {
+        if(bricksLeft == 0){
+            level++;
+        }
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 5; j++) {
                 if (brick[i][j].hitBottom(x1, y1)) {
                     ball.setDirectionY(-ball.getDirectionY());
+                    score += 50;
+                    bricksLeft--;
                 }
                 if (brick[i][j].hitTop(x1, y1)) {
                     ball.setDirectionY(-ball.getDirectionY());
+                    score += 50;
+                    bricksLeft--;
                 }
                 if (brick[i][j].hitLeft(x1, y1)) {
                     ball.setDirectionX(-ball.getDirectionX());
+                    score += 50;
+                    bricksLeft--;
                 }
                 if (brick[i][j].hitRight(x1, y1)) {
                     ball.setDirectionX(-ball.getDirectionX());
+                    score += 50;
+                    bricksLeft--;
                 }
             }
         }
@@ -111,9 +107,13 @@ public class GamePanel extends JPanel implements Constants {
     public void checkIfOut(int y1) {
         if (y1 > Constants.HEIGHT - 90) {
             numberOfLives--;
+            score -= 100;
             if (numberOfLives == 0) {
+
                 createBricks();
                 numberOfLives = PLAYER_LIVES;
+                score = 0;
+                isRunning = false;
             }
             ball.reset();
             repaint();
@@ -129,6 +129,7 @@ public class GamePanel extends JPanel implements Constants {
 
     public void draw() {
         // Background
+        //g.drawImage(ImageLoader.loadImage("Images/bckg.jpg"), 0, 0, 800, 600, null);
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, Constants.WIDTH, Constants.HEIGHT);
 
@@ -149,10 +150,33 @@ public class GamePanel extends JPanel implements Constants {
 
         Graphics2D g2 = (Graphics2D) g;
 
+        g2.setColor(Color.BLACK);
         g2.drawImage(image, 0, 0, Constants.WIDTH, Constants.HEIGHT, null);
-
+        g2.drawString("Lives: " + numberOfLives, 10, getHeight() - (getHeight() / 10));
+        g2.drawString("Score: " + score, 10, getHeight() - (2 * (getHeight() / 10)) + 25);
+        g2.drawString("Level: " + level, 10, getHeight() - (3 * (getHeight() / 10)) + 50);
         g2.dispose();
 
+
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            //Update
+            update();
+            //Draw
+            draw();
+            //displayImage
+
+            checkIfOut(ball.getY());
+            repaint();
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException ie) {
+
+            }
+        }
     }
 
     private class myMouseMotionListener implements MouseMotionListener {
@@ -169,17 +193,4 @@ public class GamePanel extends JPanel implements Constants {
         }
     }
 
-    private class BoardListener extends KeyAdapter {
-        @Override
-        public void keyPressed(KeyEvent ke) {
-            int key = ke.getKeyCode();
-            if (key == KeyEvent.VK_SPACE) {
-                if (running == true) {
-                    running = false;
-                } else {
-                    running = true;
-                }
-            }
-        }
-    }
 }
